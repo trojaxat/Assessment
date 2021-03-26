227665ef366ba4b93826e8635673e9814a768914
trojaxat <dan.axford89@gmail.com>
2021-03-26 22:44:18 +0100

    as

--- MeanSquaredError.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def calculate(actual, pred):
-    actual, pred = np.array(actual), np.array(pred)
-    return np.square(np.subtract(actual, pred)).mean()
--- README.md
+++
@@ -1,2 +0,0 @@
-# assesment
- spiced academy assessment
